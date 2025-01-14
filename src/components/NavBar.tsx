7d7e2db611e7f31d578f723879467c016dee403a
Gulam Anas <g.anas786@gmail.com>
2025-01-14 13:46:34 +0530

    logout button navigate to login

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,13 +2,14 @@ import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUserCircle } from "@fortawesome/free-solid-svg-icons";
 import "./NavBar.css";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 
 const Navbar = ({ }) => {
     const [showDropdown, setShowDropdown] = useState(false);
-
+    const navigate = useNavigate();
     const handleLogout = () => {
-        alert("You have logged out!"); // Replace with actual logout logic
+        navigate('/login')
+        // alert("You have logged out!"); 
     };
     const storeName = 'Food Mania'
     const username = 'Arshad'
